b246aa7663fbd5e8230ff50d1383bee6a453a808
guizim03 <guilherme.amaral26@escola.pr.gov.br>
2025-10-10 08:04:10 -0300

    salvar

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criarcartao(
+'progamaçao',
+'o que e python?',
+'o python e uma linguagem de progamaçao'
+)
+criacartao( 
+'geografia,'
+'qual a capital da frança?',
+'a capital da frança e paris'
+)
+criarcartao(
+    ' programaçao',
+    ' o que e uma funçao?',
+    ' uma funçao e um bloco de codigo que executa alguma tarefa'
+)
