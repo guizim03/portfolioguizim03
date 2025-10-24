b663ac8e1e08586c9140da9790c5b8b56eccb466
guizim03 <guilherme.amaral26@escola.pr.gov.br>
2025-10-24 10:43:47 -0300

    salve

--- perguntas.js
+++ perguntas.js
@@ -13,3 +13,8 @@ criarcartao(
     ' o que e uma funçao?',
     ' uma funçao e um bloco de codigo que executa alguma tarefa'
 )
+criacartao(
+    'lingua inglesa',
+    'como se diz oi em ingles
+    'oi em ingles e hi (rai)
+)
